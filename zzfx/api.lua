8c90e12c5b83dd39ebc7a6c0fd28aafd8e7b1eac
Justin Walsh <contact.me@thejustinwalsh.com>
2021-11-25 05:41:02 -0700

    fix: api wrapper build_sample

--- zzfx/api.lua
+++ zzfx/api.lua
@@ -6,18 +6,18 @@ function M.play(...)
 end
 
 -- set the samplerate, defaults to 44100
-function M.samplerate(...)
-  return zzfx_ext.samplerate(...)
+function M.samplerate(rate)
+  zzfx_ext.samplerate(rate)
 end
 
 -- play a sample returnd from zzfx_ext.build_sample
-function M.play_sample(...)
-  zzfx_ext.play_sample(...)
+function M.play_sample(sample)
+  zzfx_ext.play_sample(sample)
 end
 
 -- build a sample from a zzfx param list
 function M.build_sample(...)
-  zzfx_ext.build_sample(...)
+  return zzfx_ext.build_sample(...)
 end
 
 return M
